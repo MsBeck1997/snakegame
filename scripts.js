ee0b985747679337582494079ae4a7a9eb57d9eb
Sarah Beck <sarah.lizzie.beck@gmail.com>
2020-06-10 08:21:21 -0600

    added initial setup, and started variables in javascript

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// sets canvas on index to 'canvas', then sets context to '2d' to render
+const canvas = document.getElementById('snake')
+const canvasContext = canvas.getContext('2d')
+
+// set a 'unit' for measurement game-wide
+const box = 32px
